b5f4941c31a5940fc9677e1da8f932da8d8de088
MTesseracT <mtesseracttech@gmail.com>
2017-09-06 20:07:08 +0200

    Fixed everything for windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,35 @@
-#include <iostream>
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
-int main()
-{
+#include <iostream>
+
+int main() {
     glfwInit();
 
-    //uint32_t extensionCount = 0;
-    //vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-    //std::cout << extensionCount << " extensions supported" << std::endl;
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    glm::vec3 testVec(0,1,1);
-    std::cout << testVec.x << testVec.y << testVec.z << std::endl;
+    std::cout << extensionCount << " extensions supported" << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
 
     return 0;
 }
