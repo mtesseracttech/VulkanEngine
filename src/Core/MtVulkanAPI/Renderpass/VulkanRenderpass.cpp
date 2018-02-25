b65ff18e7a608fbb38abc00fbf8b6a015efc8e1d
MTesseracT <mtesseracttech@gmail.com>
2018-02-25 18:57:57 +0100

    Some additions to the swapchain and a small start to the renderpass.

---
+++ src/Core/MtVulkanAPI/Renderpass/VulkanRenderpass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mtesseract on 25-2-18.
+//
+
+#include "VulkanRenderpass.hpp"
---
+++ src/Core/MtVulkanAPI/Renderpass/VulkanRenderpass.hpp
@@ -0,0 +1,15 @@
+//
+// Created by mtesseract on 25-2-18.
+//
+
+#ifndef VULKANENGINE_VULKANRENDERPASS_HPP
+#define VULKANENGINE_VULKANRENDERPASS_HPP
+
+
+class VulkanRenderpass
+{
+
+};
+
+
+#endif //VULKANENGINE_VULKANRENDERPASS_HPP
